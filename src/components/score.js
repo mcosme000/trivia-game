6f2aead9832551067993ea6a0273efaafae4ea96
Maria Cosmen <mcosme00@gmail.com>
2023-08-08 17:26:53 +0900

    Add lotties animations

--- src/components/score.js
+++ src/components/score.js
@@ -1,5 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import { finishGame, resetScore, resetCurrentQuestion, updateShowScore } from "../slices/triviaSlice";
+import Lottie from "react-lottie";
+import AnimationData from "../lotties/celebration.json"
+import GameLost from "../lotties/bw.json"
 
 const Score = () => {
   const dispatch = useDispatch()
@@ -11,13 +14,53 @@ const Score = () => {
     dispatch(finishGame())
     dispatch(updateShowScore())
   }
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  const gameLost = {
+    loop: true,
+    autoplay: true,
+    animationData: GameLost,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
-    <div className="h-full p-8 flex flex-col justify-center items-center">
-      { score === 0 && <p>Ups...! You didn't get any answer correct. </p>}
-      { score > 0 && <p>You got <strong>{score}</strong> correct answer{score > 1 && 's'}!</p>}
-      <button onClick={handleFinishGame}
-      className="mt-3 bg-yellow hover:bg-yellow-dark text-sm px-8 py-2 rounded-md font-bold tracking-wide cursor-pointer">
-        Play again</button>
+    <div className="flex justify-center flex-col items-center h-full">
+      <div className="w-full h-full relative">
+        { score === 0 &&
+           <Lottie
+            options={gameLost}
+            height={200}
+            width={300}
+            isClickToPauseDisabled
+          />
+        }
+        { score > 0 &&
+          <Lottie
+            options={defaultOptions}
+            height={"100%"}
+            width={"100%"}
+            isClickToPauseDisabled
+          />
+        }
+      </div>
+      <div className="absolute flex flex-col justify-center items-center">
+        { score === 0 && <p>Ups...! You didn't get any answer correct. </p>}
+        { score > 0 && <p>You got <strong>{score}</strong> correct answer{score > 1 && 's'}!</p>}
+        <button onClick={handleFinishGame}
+        className="mt-3 bg-yellow hover:bg-yellow-dark text-sm px-8 py-2 rounded-md font-bold tracking-wide cursor-pointer">
+          Play again
+        </button>
+      </div>
     </div>
   )
 }
