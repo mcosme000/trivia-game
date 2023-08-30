f0208b3ed0ec892abaca2c2c2592e3ea78238ccc
Maria Cosmen <mcosme00@gmail.com>
2023-08-30 23:26:05 +0900

    Move API logic to hintService

--- src/components/hint.js
+++ src/components/hint.js
@@ -1,23 +1,7 @@
 import React from 'react'
-import OpenAI from 'openai';
-const apikey = process.env.REACT_APP_API_KEY
-
-const openai = new OpenAI({
-  apiKey: apikey,
-  dangerouslyAllowBrowser: true,
-});
-
-const getHint = async (question) => {
-  console.log(`Getting a hint about this question: ${question}`);
-  const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": `Can you give me a hint for this question? ${question}`}],
-  });
-  console.log(chatCompletion.choices[0].message.content);
-}
+import getHint from '../services/hintService';
 
 const Hint = (props) => {
-
   return (
     <div>
       <button onClick={() => getHint(props.question)}>Get hint!</button>
