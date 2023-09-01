39b22df3ff7d22b5ac566c852a0d1b3c3425d143
Maria Cosmen <mcosme00@gmail.com>
2023-09-01 17:13:53 +0900

    Comment Hint related code

--- src/slices/hintSlice.js
+++ src/slices/hintSlice.js
@@ -1,40 +1,40 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import getHint from "../services/hintService";
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import getHint from "../services/hintService";
 
-export const fetchHint = createAsyncThunk('hint/getHint', async (data) => {
-  const hint = await getHint(data);
-  return hint;
-})
+// export const fetchHint = createAsyncThunk('hint/getHint', async (data) => {
+//   const hint = await getHint(data);
+//   return hint;
+// })
 
-const hintSlice = createSlice({
-  name: "hint",
-  initialState: {
-    hint: "",
-    status: "",
-    error: null
-  },
-  reducers: {
-    updateHint(state, action) {
-      console.log(action);
-    }
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchHint.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchHint.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.hint = action.payload;
-        console.log("Hint from the extrareducer");
-        console.log(state.hint);
-      })
-      .addCase(fetchHint.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-  }
-})
+// const hintSlice = createSlice({
+//   name: "hint",
+//   initialState: {
+//     hint: "",
+//     status: "",
+//     error: null
+//   },
+//   reducers: {
+//     updateHint(state, action) {
+//       console.log(action);
+//     }
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchHint.pending, (state) => {
+//         state.status = "loading";
+//       })
+//       .addCase(fetchHint.fulfilled, (state, action) => {
+//         state.status = "succeeded";
+//         state.hint = action.payload;
+//         console.log("Hint from the extrareducer");
+//         console.log(state.hint);
+//       })
+//       .addCase(fetchHint.rejected, (state, action) => {
+//         state.status = "failed";
+//         state.error = action.error.message;
+//       })
+//   }
+// })
 
 
-export const hintSliceReducer = hintSlice.reducer
+// export const hintSliceReducer = hintSlice.reducer
