a090f66141c62d5580dff879ee1d94fef96f6dc9
Maria Cosmen <mcosme00@gmail.com>
2023-09-02 21:58:46 +0900

    Redo GPT API

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { triviaSliceReducer } from "../slices/triviaSlice"
-// import { hintSliceReducer } from "../slices/hintSlice";
+import { hintSliceReducer } from "../slices/hintSlice";
 
 const store = configureStore({
   reducer: {
     trivia: triviaSliceReducer,
-    // hint: hintSliceReducer
+    hint: hintSliceReducer
   }
 })
 
